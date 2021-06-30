dd73a836f435570de199bd7fb7360a1660f9bc5d
Jacquelyn Vinson <jacqvinson@gmail.com>
2021-06-30 08:11:45 +0000

    Update __init__.py

--- abdbeam/__init__.py
+++ abdbeam/__init__.py
@@ -1,5 +1,4 @@
 from .core import Section, Point, Segment, Load
-from .materials import (Material, Isotropic, ShearConnector, PlyMaterial,
-                        Laminate)
+from .materials import (Material, Isotropic, ShearConnector, PlyMaterial, Laminate)
 from .plots import plot_section, plot_section_loads
 
